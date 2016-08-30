7e4fa88072bb4124f275c4facece2818baac7fd6
nischay30 <nischay.goyal@gmail.com>
2016-08-30 10:58:53 +0530

    Fourth Commit

--- Second/player.js
+++ Second/player.js
@@ -1,8 +1,8 @@
 $(function(){
 
-	var startpage=0;
-	var startlimit=10;
-	var lastpage=100000;
+	var counter=0;
+	var limit=20;
+	var length=0;
 
 	$countrytable= $('#countrytable');
 	$modalbody=$('#modalbody');
@@ -10,17 +10,41 @@ $(function(){
 	$modal=$('.modal');
 
 
-	$("#first").on('click',function(){
+//Pager List
+$first=$('#first');
+$next=$('#next');
+$last=$('#last');
+$prev=$('#prev');
+
+
+function hidePager()
+{
+	$first.hide();
+	$prev.hide();
+	$next.hide();
+	$last.hide();
+};
+
+
+	getTable();
+	$('#viewAll').on('click',getTable);		//call to get the first view of table
+	$('.table-condensed').on('click','.glyphicon-trash',deleteRow);  //delete call
+	$('.table-condensed').on('click','.glyphicon-edit',showRow);		//update call
+
+
+	$first.on('click',function(){
 		getNextTable('first');
 	});
 
-	$("#next").on('click',function(){
+	$next.on('click',function(){
 		getNextTable('next');
 	});
-	$("#previous").on('click',function(){
+
+	$prev.on('click',function(){
 		getNextTable('prev');
 	});
-	$("#last").on('click',function(){
+
+	$last.on('click',function(){
 		getNextTable('last');
 	});
 
@@ -28,97 +52,168 @@ $(function(){
 		$('#search').on('click',getRow);			//to search by country Name
 		$('#form1').on('submit',addRow);			//to add a row
 
-		(function getTable(startp)
+
+
+
+
+
+
+		function showRow()
 		{
+			$rowId=$(this.id);
+			$rowId=$rowId.selector.trim().replace('e','r');
+			$tdId=$('#'+this.id+'d');
+			$('.updaterow').hide();
+			$('#'+this.id+'r').fadeToggle();
+			$tdId.html("<div><label>Country Name</label><input type='text' required id='updatedCountryName' value='"+$('#'+$rowId).find('td:first')[0].innerText+
+				"'><br><label>Gold Medals</label><input type='text' id='updatedGoldMedals' value='"+$('#'+$rowId).find('td:nth-child(2)')[0].innerText+
+				"'><br><label>Silver Medals</label> <input type='text' id='updatedSilverMedals' value='"+$('#'+$rowId).find('td:nth-child(3)')[0].innerText+
+				"'><br><label>Bronze Medals</label> <input type='text' id='updatedBronzeMedals' value='"+$('#'+$rowId).find('td:nth-child(4)')[0].innerText+
+				"'><br><label>Total Medals</label> <input type='text' id='updatedTotalMedals' value='"+$('#'+$rowId).find('td:nth-child(5)')[0].innerText+"'</div>");
+			$tdId.append("<button class='btn-info btn-large' id='updateBtn'>Update</button><button class='btn-danger btn-large' id='closeBtn'>Close</button>");
+			$('#updateBtn').on('click',updateRow);
+			$('#closeBtn').on('click',function(){
+				$('.updaterow').hide();
+
+			});
+		};
+
+
+
+//Put Command
+function updateRow()
+{
+	$updatedCountryName=$('#updatedCountryName').val();
+	$updatedGoldMedals=$('#updatedGoldMedals').val();
+	$updatedSilverMedals=$('#updatedSilverMedals').val();
+	$updatedBronzeMedals=$('#updatedBronzeMedals').val();
+	$updatedTotalMedals=$('#updatedTotalMedals').val();
+	var countryid=$rowId.replace('r','');
+	var countries={"countryName": $updatedCountryName,"gold": $updatedGoldMedals,"silver": $updatedSilverMedals,"bronze": $updatedBronzeMedals,"total":$updatedTotalMedals};
+	$.ajax({
+		type:'PUT',
+		data:countries,
+		url:'http://localhost:3000/countries/'+countryid,
+		success:function(){
+			$('#'+countryid+'ed').fadeToggle();			
+
+			alert("Updated");
+			$('#'+countryid+'r').html("<td class='country'><a href='#'>"+$updatedCountryName+
+				"</a></td><td>"+$updatedGoldMedals+
+				"</td><td>"+$updatedSilverMedals+
+				"</td><td>"+$updatedBronzeMedals+
+				"</td><td>"+$updatedTotalMedals+
+				"</td><td><span class='glyphicon glyphicon-edit' id='"+countryid+"e'>&nbsp</span><span class='glyphicon glyphicon-trash' id='"+countryid+"'></span></td></tr>");
+			$('#'+countryid+'er').html("<td colspan='6' id='"+countryid+"ed'></td>");
+			$('#'+countryid+'er').hide();
+		}	
+	})
+};
+
+
+		//function is used to get the length of the data
+		(function getLength()
+		{
+
 			$.ajax({
 				type:'GET',
-				url:'http://localhost:3000/countries/?_start='+startpage+'&_limit='+startlimit,
+				url:'http://localhost:3000/countries',
 				success:function(data){
-					$countrytable.empty();
-					$countrytable.append("<thead><tr><th>Country Name</th><th>Gold Medals</th> <th>Silver Medals</th> <th>Bronze Medals</th> <th>Total Medals</th><th>Operations</th></tr></thead>")
+					length=data.length;
+				}
+
+			});
+		})();
 
 
-					$.each(data,function(i,country){
-						$countrytable.append("<tr id='"+country.id+"r'><td class='country'><a href='#'>"+country.countryName.toUpperCase()+
-							"</a></td><td>"+country.gold+
-							"</td><td>"+country.silver+
-							"</td><td>"+country.bronze+
-							"</td><td>"+country.total+
-							"</td><td><span class='glyphicon glyphicon-edit' id='"+country.id+"e'>&nbsp</span><span class='glyphicon glyphicon-trash' id='"+country.id+"'></span></td></tr>");
-						$countrytable.append("<tr class='updaterow' id='"+country.id+"er'><td colspan='6' id='"+country.id+"ed'></td></tr>");
-						$('#'+country.id+'er').hide();
-					});
+//Starting funciton to show the table
+function getTable()
+{
+	$first.show();
+	$prev.show();
+	$next.show();
+	$last.show();
 
-			$('.glyphicon-trash').on('click',deleteRow);  //delete call
-			$('.glyphicon-edit').on('click',showRow);
+	counter=0;
+	$.ajax({
+		type:'GET',
+		url:'http://localhost:3000/countries/?_start='+counter+'&_limit='+limit,
+		success:function(data)
+		{
+			$countrytable.empty();
+			appendTableHeader(data);
 		}
 
 	});
-		})();
+};
+
+
+//function used to append table header 
+function appendTableHeader(data)
+{
+	$countrytable.append("<thead><tr><th>Country Name</th><th>Gold Medals</th> <th>Silver Medals</th> <th>Bronze Medals</th> <th>Total Medals</th><th>Operations</th></tr></thead>");		
+	appendTableRows(data);
+
+};
+
+//fucntion to append Table Rows
+function appendTableRows(data)
+{
+	$.each(data,function(i,country){
+		$countrytable.append("<tr id='"+country.id+"r'><td class='country'><a href='#'>"+country.countryName+
+			"</a></td><td>"+country.gold+
+			"</td><td>"+country.silver+
+			"</td><td>"+country.bronze+
+			"</td><td>"+country.total+
+			"</td><td><span class='glyphicon glyphicon-edit' id='"+country.id+"e'>&nbsp</span><span class='glyphicon glyphicon-trash' id='"+country.id+"'></span></td></tr>");
+		$countrytable.append("<tr class='updaterow' id='"+country.id+"er'><td colspan='6' id='"+country.id+"ed'></td></tr>");
+		$('#'+country.id+'er').hide();
+	});
+};
+
 
 //GET Command for Next All
 function getNextTable(pager)
-
 {
-	console.log(pager);
 	if(pager==='next')
-	{
-		startpage=startpage+10;
-	}
+		counter=counter+20;
 	else if(pager==='prev')
-		startpage=startpage-10;
+		counter=counter-20;
 	else if(pager==='first')
-		startpage=0;
+		counter=0;
 	else if(pager==='last')
-		startpage=lastpage-10;
+		counter=length-20;
 
-
-	if(startpage<0)
-	{
+	if(counter<0)
+	{	
 		alert("This is the First Page");
-		startpage=startpage+10;
+		counter=counter+20;
 	}
-	else if(startpage>=lastpage)
+	else if(counter>=length)
 	{
 		alert("This is the Last Page");
-		startpage=startpage-10;
+		counter=counter-20;
 	}
 	else
 	{
 		$.ajax({
 			type:'GET',
-			url:'http://localhost:3000/countries/?_start='+startpage+'&_limit='+startlimit,
+			url:'http://localhost:3000/countries/?_start='+counter+'&_limit='+limit,
 			success:function(data){
 				$countrytable.empty();
-				$countrytable.append("<thead><tr><th>Country Name</th><th>Gold Medals</th> <th>Silver Medals</th> <th>Bronze Medals</th> <th>Total Medals</th><th>Operations</th></tr></thead>")
-
-
-				$.each(data,function(i,country){
-					$countrytable.append("<tr id='"+country.id+"r'><td class='country'><a href='#'>"+country.countryName.toUpperCase()+
-						"</a></td><td>"+country.gold+
-						"</td><td>"+country.silver+
-						"</td><td>"+country.bronze+
-						"</td><td>"+country.total+
-						"</td><td><span class='glyphicon glyphicon-edit' id='"+country.id+"e'>&nbsp</span><span class='glyphicon glyphicon-trash' id='"+country.id+"'></span></td></tr>");
-					$countrytable.append("<tr class='updaterow' id='"+country.id+"er'><td colspan='6' id='"+country.id+"ed'></td></tr>");
-					$('#'+country.id+'er').hide();
-				});
-
-			$('.glyphicon-trash').on('click',deleteRow);  //delete call
-			$('.glyphicon-edit').on('click',showRow);
-		}
-
-	});
+				appendTableHeader(data);
+			}
+		});
 	}
 };
 
 
+//complete
 //Post Command to add row
 function addRow(e)
 {
 	e.preventDefault();
-	console.log("time")
-	$inputName=$('#inputName').val().toLowerCase();
+	$inputName=$('#inputName').val();
 	$inputGold=$('#inputGold').val();
 	$inputSilver=$('#inputSilver').val();
 	$inputBronze=$('#inputBronze').val();
@@ -128,64 +223,24 @@ function addRow(e)
 		type:'POST',
 		data:countries,
 		url:'http://localhost:3000/countries',
-		success:function(){
+		success:function(country){
+			var data1=[country];
+
+			length +=1;
 			$modal.modal('toggle');
 			$('#form1')[0].reset();
+			appendTableRows(data1);
 		}	
 	})
-
-};
-
-function showRow()
-{
-
-	$rowId=$(this.id);
-	$rowId=$rowId.selector.trim().replace('e','r');
-	
-	$tdId=$('#'+this.id+'d');
-
-	$('.updaterow').hide();
-
-	$('#'+this.id+'r').fadeToggle();
-	$tdId.html("<div><label>Country Name</label><input type='text' id='updatedCountryName' value='"+$('#'+$rowId).find('td:first')[0].innerText+
-		"'><br><label>Gold Medals</label><input type='text' id='updatedGoldMedals' value='"+$('#'+$rowId).find('td:nth-child(2)')[0].innerText+
-		"'><br><label>Silver Medals</label> <input type='text' id='updatedSilverMedals' value='"+$('#'+$rowId).find('td:nth-child(3)')[0].innerText+
-		"'><br><label>Bronze Medals</label> <input type='text' id='updatedBronzeMedals' value='"+$('#'+$rowId).find('td:nth-child(4)')[0].innerText+
-		"'><br><label>Total Medals</label> <input type='text' id='updatedTotalMedals' value='"+$('#'+$rowId).find('td:nth-child(5)')[0].innerText+"'</div>");
-	$tdId.append("<button class='btn-danger btn-large' id='updateBtn'>Update</button>");
-
-
-	$('#updateBtn').on('click',updateRow);
 };
 
 
-
-function updateRow()
-{
-	$updatedCountryName=$('#updatedCountryName').val().toLowerCase();
-	$updatedGoldMedals=$('#updatedGoldMedals').val();
-	$updatedSilverMedals=$('#updatedSilverMedals').val();
-	$updatedBronzeMedals=$('#updatedBronzeMedals').val();
-	$updatedTotalMedals=$('#updatedTotalMedals').val();
-	var countryid=$rowId.replace('r','');
-
-	var countries={"countryName": $updatedCountryName,"gold": $updatedGoldMedals,"silver": $updatedSilverMedals,"bronze": $updatedBronzeMedals,"total":$updatedTotalMedals};
-	$.ajax({
-		type:'PUT',
-		data:countries,
-		url:'http://localhost:3000/countries/'+countryid,
-		success:function(){
-			$('#'+countryid+'ed').hide();			
-			alert("Updated");
-		}	
-	})
-
-};
-
-
+//complete
 //Get Command to search One row
 function getRow(){
-	$countryName=$('#countryName').val().toLowerCase();
+	$countryName=$('#countryName').val();
+	hidePager();
+
 	$countrytable.empty();
 	if($countryName.trim().length===0)
 	{
@@ -203,30 +258,14 @@ function getRow(){
 				}
 				else
 				{
-					$countrytable.append("<thead><tr><th>Country Name</th><th>Gold Medals</th> <th>Silver Medals</th> <th>Bronze Medals</th> <th>Total Medals</th><th>Operations</th></tr></thead>")
-					$.each(data,function(i,country){
-
-						$countrytable.append("<tr id='"+country.id+"r'><td class='country'><a href='#'>"+country.countryName.toUpperCase()+
-							"</a></td><td>"+country.gold+
-							"</td><td>"+country.silver+
-							"</td><td>"+country.bronze+
-							"</td><td>"+country.total+
-							"</td><td><span class='glyphicon glyphicon-edit' id='"+country.id+"e'>&nbsp</span><span class='glyphicon glyphicon-trash' id='"+country.id+"'></span></td></tr>");
-						$countrytable.append("<tr><td colspan='6' id='"+country.id+"ed'></td></tr>");
-						$('#'+country.id+'ed').hide();
-					});
-			$('.glyphicon-trash').on('click',deleteRow);  //delete call
-			$('.glyphicon-edit').on('click',showRow);
-
-		}
-	}
-});
+					appendTableHeader(data);
+				}
+			}
+		});
 	}
 };
 
-
-
-
+//complete
 //Delete Row Function
 function deleteRow()
 {
@@ -236,10 +275,8 @@ function deleteRow()
 		type:'DELETE',
 		url:'http://localhost:3000/countries/'+this.id,
 		success:function(){
-			alert("Deleted");
-
+			length -=1;
 		}
 	});
 }
-
 });
